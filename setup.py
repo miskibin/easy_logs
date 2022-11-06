3cb203a14dd0f540466cc44078b526a2f556ad2a
michalskibinski109 <michalskibinski109@gmail.com>
2022-11-06 12:13:00 +0100

    improved setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 # make path to readme to be readable to github worker
 
-readme_path = Path(__file__).parent / "README.md"
+readme_path = Path().cwd() / "README.md"
 with open(readme_path, encoding="utf-8") as f:
     long_description = f.read()
 
