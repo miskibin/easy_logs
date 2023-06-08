553b3a20a565b86349eb6c8c7e267714d5dd3c58
michalskibinski109 <michalskibinski109@gmail.com>
2023-06-08 09:11:05 +0200

    Update readme

--- easy_logs/__init__.py
+++ easy_logs/__init__.py
@@ -1,24 +1,20 @@
 """
-This module provides a convenient way to create loggers with colored logs and filtering for IPython/Jupyter Notebook cells.
+This module provides a convenient way to create loggers with colored logs.
 
-The main function `get_logger` can be used to obtain a logger with various configurations, including colored logs, saving logs to a file, and specifying log formatting. Additionally, it supports predefined configurations that simplify logger setup.
+The main function get_logger can be used to obtain a logger with various configurations, 
+including colored logs, saving logs to a file, and specifying log formatting. Additionally,
+it supports predefined configurations that simplify logger setup.
 
 Example usage:
-    logger = get_logger(logger_name="my_logger", lvl=20, file_name="logs.log", format="%(message)s (%(filename)s:%(lineno)d)")
+logger = get_logger(predefined="professional")
+logger.info("Everything is OK!")
+>>> 2021-01-01 12:00:00,000 - INFO - demo.py:1 - Everything is OK!
 
-Predefined Configurations:
-    - `simple`: A simple logger that works like `print()` but with colors.
-    - `professional`: A logger that saves logs to a file and displays additional information such as time, filename, line number, and log level.
-    - `default`: Default logger configuration.
-
-Note: To use the predefined configurations, specify the `predefined` argument with the desired configuration name.
-
-For more information, refer to the docstring of the `get_logger` function.
+For more information, refer to the docstring of the get_logger function.
 
 Author: Michał Skibiński
 Contact: mskibinski109@gmail.com
 """
-
 from .main import get_logger, ColoredFormatter
 
 __all__ = ["get_logger", "ColoredFormatter"]
